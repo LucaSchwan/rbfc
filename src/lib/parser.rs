e1ecb8c3da09a17f81b60d3d9a1fdc9ca5d86be1
ehrenschwan <luca@ehrenschwan.dev>
2024-02-02 16:44:31 +0100

    fix: added parser errors

--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -1,4 +1,14 @@
+use thiserror::Error;
+
 use super::lexer;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum ParserError {
+    #[error("Unmatched bracket at position {0}")]
+    UnmatchedBracket(usize),
+    #[error("Unexpected end of file at position {0}, expected closing bracket at position {1}")]
+    UnexpectedEof(usize, usize),
+}
 
 #[derive(Debug)]
 pub struct Parser {
@@ -12,32 +22,100 @@ impl Parser {
         }
     }
 
-    pub fn parse(&mut self) -> Vec<lexer::Token> {
+    pub fn parse(&mut self) -> Result<Vec<lexer::Token>, ParserError> {
         let mut jump_stack = Vec::new();
         let mut ops = Vec::new();
-        let mut curr_op = 0;
+        let mut loc = 0;
 
         loop {
             let mut token = self.lexer.next_token();
             match token.token_type {
                 lexer::TokenType::Eof => break,
                 lexer::TokenType::OpenBracket => {
-                    jump_stack.push(curr_op);
+                    jump_stack.push(loc);
                     ops.push(token);
                 }
                 lexer::TokenType::CloseBracket => {
-                    let jump = jump_stack.pop().expect("No matching open bracket");
+                    let jump = jump_stack.pop().ok_or(ParserError::UnmatchedBracket(loc))?;
                     token.size = Some(jump);
-                    ops[jump].size = Some(curr_op);
+                    ops[jump].size = Some(loc + 1);
                     ops.push(token);
                 }
                 _ => {
                     ops.push(token);
                 }
             }
-            curr_op += 1;
+            loc += 1;
         }
 
-        ops
+        if !jump_stack.is_empty() {
+            return Err(ParserError::UnexpectedEof(
+                loc,
+                jump_stack.pop().expect("Should be some location"),
+            ));
+        }
+
+        Ok(ops)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        let input = String::from("++[->+<]");
+        let mut parser = Parser::new(input);
+        let result = parser.parse().unwrap();
+        assert_eq!(
+            result,
+            vec![
+                lexer::Token {
+                    token_type: lexer::TokenType::Plus,
+                    size: Some(2)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::OpenBracket,
+                    size: Some(7)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::Minus,
+                    size: Some(1)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::ShiftRight,
+                    size: Some(1)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::Plus,
+                    size: Some(1)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::ShiftLeft,
+                    size: Some(1)
+                },
+                lexer::Token {
+                    token_type: lexer::TokenType::CloseBracket,
+                    size: Some(1)
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_parser_unmatched_bracket() {
+        let input = String::from("++[->+<");
+        let mut parser = Parser::new(input);
+        let result = parser.parse();
+        assert_eq!(result, Err(ParserError::UnexpectedEof(6, 1)));
+    }
+
+    #[test]
+    fn test_parser_unexpected_eof() {
+        let input = String::from("++->+<]");
+        let mut parser = Parser::new(input);
+        let result = parser.parse();
+        assert_eq!(result, Err(ParserError::UnmatchedBracket(5)));
     }
 }
